e6e57f3d58c090a55545db7ddf43ab1dfdf8d116
ritikajain528 <79856406+ritikajain528@users.noreply.github.com>
2021-05-06 11:39:36 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,56 @@
+
+class Block{
+    constructor(x,y,width,height){
+        var options={
+            
+            restitution:0,
+            friction:1,
+            //isStatic:false,
+        
+    }
+        this.body= Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+        display(){
+
+            var angle=this.body.angle;
+            var pos=this.body.position;
+            
+            Push();
+            tranlate(pos.x,pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0,0,this.width,this,height);
+            pop();
+        }
+        }
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
